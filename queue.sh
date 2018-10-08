901cc80488d04193c9e3bcfde33a8e0cde8071c3
Nikhil <nikhil.baby@happylocate.com>
2018-10-09 00:09:05 +0530

    Queue call shell script

--- queue.sh
+++ queue.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/lvabibv4r3ho/www/QuikService/
-nohup php artisan queue:work --queue=priority --sleep=1 --tries=1 --daemon &
+nohup /usr/local/bin/php artisan queue:work --queue=priority --sleep=1 --tries=1 --daemon &
